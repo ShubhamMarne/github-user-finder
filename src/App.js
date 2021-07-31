103f8d2616549bf9c4899fe85361893d22675e72
ShubhamMarne <neelmarne2012@gmail.com>
2021-08-01 01:59:27 +0530

    added calls to github api and added spinner updated App.css

--- src/App.js
+++ src/App.js
@@ -5,11 +5,27 @@ import UserItem from './components/users/UserItem';
 import Users from './components/users/Users';
 
 class App extends React.Component {
+  state = {
+    users: [],
+    loading : false
+  }
+
+  async componentDidMount(){
+    // set state for loading 
+    this.setState({loading : true});
+    const res = await fetch('https://api.github.com/users').then(function(response){
+      return response.json();
+    });
+    // reset state for loading and users
+    this.setState({users: res, loading : false});
+  }
+
+
   render() {
     return (
       <div>
         <NavBar title="Github Finder"></NavBar>
-        <Users></Users>
+        <Users loading={this.state.loading} users={this.state.users}></Users>
       </div>
     )
   };
